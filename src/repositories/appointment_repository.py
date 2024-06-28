26bd59fc1e6c4feb01de06dde01cf811eb0629d0
Isadora Moreira de Souza Silva <isadora.souza@programmers.com.br>
2024-06-28 17:29:21 -0300

    frontend working

--- src/repositories/appointment_repository.py
+++ src/repositories/appointment_repository.py
@@ -6,7 +6,7 @@ class AppointmentRepository:
     async def create(db, appointment: AppointmentModel):
         appointment_dictionary = appointment.model_dump(by_alias=True)
         appointment_dictionary["_id"] = str(appointment_dictionary["_id"])
-        result = await db['appointments'].insert_one(appointment_dictionary)
+        result = db['appointments'].insert_one(appointment_dictionary)
         appointment_dictionary['_id'] = str(result.inserted_id)
         return appointment_dictionary
     
@@ -22,11 +22,11 @@ class AppointmentRepository:
     async def update(db, appointment: UpdateAppointmentModel, id: str):
         update_appointment = {k: v for k, v in appointment.model_dump(exclude_unset=True).items()}
         db['appointments'].update_one({"_id": id}, {"$set": update_appointment})
-        appointment_to_update = await db['appointments'].find_one({"_id": id})
+        appointment_to_update = db['appointments'].find_one({"_id": id})
         appointment_to_update["_id"] = str(appointment_to_update["_id"])
         return appointment_to_update
     
     @staticmethod
     async def delete(db, id: str):
-        result = await db['appointments'].delete_one({"_id": id})
+        result = db['appointments'].delete_one({"_id": id})
         return result.deleted_count > 0
